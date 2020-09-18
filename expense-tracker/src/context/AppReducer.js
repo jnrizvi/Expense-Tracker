619518a3a6d5de2b602b495b82cb755bd5e85c47
Jawad Rizvi <jawadrizvi40@gmail.com>
2020-09-18 12:54:40 -0600

    Halfway through

---
+++ expense-tracker/src/context/AppReducer.js
@@ -0,0 +1,7 @@
+// Specify application state changes in response to certain actions to our store (context)
+export default (state, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
