3ad49027674a9f1738881a5d19624efab38da152
Jawad Rizvi <jawadrizvi40@gmail.com>
2020-09-18 19:27:30 -0600

    Now I just need to be able to add transactions

--- expense-tracker/src/context/AppReducer.js
+++ expense-tracker/src/context/AppReducer.js
@@ -1,6 +1,11 @@
 // Specify application state changes in response to certain actions to our store (context)
 export default (state, action) => {
   switch(action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
+      }
     default:
       return state;
   }
